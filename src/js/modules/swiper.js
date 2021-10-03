dba243109267e8c047d8e235a2b625d7418bce7a
Inpuls <c4.tm.gor@gmail.com>
2021-10-03 18:10:08 +0300

    swiper update

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,29 +1,4 @@
 module.exports = function () {
-	// begin gallery slider
-	new Swiper(".gallery__slider", {
-	slidesPerView: 1,
-	// spaceBetween: 10,
-	loop: true,
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	});
-	// end gallery slider
-
-	// begin planning slider
-	new Swiper(".planning__slider", {
-	slidesPerView: 4,
-	spaceBetween: 20,
-	scrollbar: {
-		el: ".swiper-scrollbar",
-		hide: false,
-		dragSize: 50,
-		draggable: true
-		},
-	});
-	// end planning slider
-
   // begin gallery slider
   new Swiper(".gallery__slider", {
     slidesPerView: 1,
@@ -39,4 +14,17 @@ module.exports = function () {
     },
   });
   // end gallery slider
+
+  	// begin planning slider
+	new Swiper(".planning__slider", {
+		slidesPerView: 4,
+		spaceBetween: 20,
+		scrollbar: {
+			el: ".swiper-scrollbar",
+			hide: false,
+			dragSize: 50,
+			draggable: true
+			},
+		});
+	// end planning slider
 } 
