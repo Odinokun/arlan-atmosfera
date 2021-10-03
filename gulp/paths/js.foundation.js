b93deaa3d581fc87c618f7bebf6bb2642a3014d1
Inpuls <c4.tm.gor@gmail.com>
2021-10-03 13:57:03 +0300

    update

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -2,6 +2,7 @@
 
 module.exports = [
   './node_modules/jquery/dist/jquery.min.js',
+  './node_modules/swiper/swiper-bundle.js',
   './node_modules/svg4everybody/dist/svg4everybody.min.js'
 
   // include jQuery UI
--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,6 +1,9 @@
 module.exports = function () {
 	  // begin gallery slider
 	  new Swiper(".gallery__slider", {
+		slidesPerView: 1,
+		// spaceBetween: 10,
+		loop: true,
 		navigation: {
 		  nextEl: ".swiper-button-next",
 		  prevEl: ".swiper-button-prev",
