c4a41733984d0807ece29069d5d67f8bab893988
odinokun <odinokun@gmail.com>
2021-10-03 17:17:47 +0300

    added fade effect

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,13 +1,17 @@
 module.exports = function () {
-	  // begin gallery slider
-	  new Swiper(".gallery__slider", {
-		slidesPerView: 1,
-		// spaceBetween: 10,
-		loop: true,
-		navigation: {
-		  nextEl: ".swiper-button-next",
-		  prevEl: ".swiper-button-prev",
-		},
-	  });
-	  // end gallery slider
+  // begin gallery slider
+  new Swiper(".gallery__slider", {
+    slidesPerView: 1,
+    loop: true,
+    speed: 500,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  // end gallery slider
 } 
